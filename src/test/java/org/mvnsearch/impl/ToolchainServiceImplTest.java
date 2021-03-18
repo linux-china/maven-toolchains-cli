908b61a9ab90f7de22a168da52a956d38a50bf2f
linux_china <libing.chen@gmail.com>
2021-03-18 02:25:00 -0700

    ToolchainServiceImplTest

---
+++ src/test/java/org/mvnsearch/impl/ToolchainServiceImplTest.java
@@ -0,0 +1,17 @@
+package org.mvnsearch.impl;
+
+import org.junit.jupiter.api.Test;
+import org.mvnsearch.model.Toolchain;
+
+
+public class ToolchainServiceImplTest {
+
+    private final ToolchainServiceImpl toolchainService = new ToolchainServiceImpl();
+
+    @Test
+    public void testFindAllToolchains() {
+        for (Toolchain toolchain : toolchainService.findAllToolchains()) {
+            System.out.println(toolchain.findVersion());
+        }
+    }
+}
