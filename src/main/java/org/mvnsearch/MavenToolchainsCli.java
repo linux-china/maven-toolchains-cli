fd65d43dfb8126dab561573c92759317d02058a5
linux_china <libing.chen@gmail.com>
2021-03-18 01:44:22 -0700

    MavenToolchainsCli

---
+++ src/main/java/org/mvnsearch/MavenToolchainsCli.java
@@ -0,0 +1,17 @@
+package org.mvnsearch;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class MavenToolchainsCli {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext app = new SpringApplicationBuilder(MavenToolchainsCli.class)
+                .bannerMode(Banner.Mode.OFF)
+                .run(args);
+        System.exit(SpringApplication.exit(app));
+    }
+}
