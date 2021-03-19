fcba5bfa95e859794a8e1aa67a1574678e288c64
linux_china <libing.chen@gmail.com>
2021-03-18 17:56:28 -0700

    Add JacksonXmlRootElement

--- src/main/java/org/mvnsearch/model/Toolchains.java
+++ src/main/java/org/mvnsearch/model/Toolchains.java
@@ -1,10 +1,12 @@
 package org.mvnsearch.model;
 
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@JacksonXmlRootElement(localName = "toolchains")
 public class Toolchains {
     @JacksonXmlElementWrapper(useWrapping = false)
     private List<Toolchain> toolchain = new ArrayList<>();
