28463658cc83073912c5a92a07b70b64602cca4a
linux_china <libing.chen@gmail.com>
2021-03-18 17:30:18 -0700

    Add downloadAndExtract

--- src/main/java/org/mvnsearch/service/AdoptOpenJDKService.java
+++ src/main/java/org/mvnsearch/service/AdoptOpenJDKService.java
@@ -3,7 +3,11 @@ package org.mvnsearch.service;
 
 import org.mvnsearch.model.jdk.JdkRelease;
 
+import java.io.File;
+
 public interface AdoptOpenJDKService {
 
     JdkRelease[] findReleases(String version) throws Exception;
+
+    File downloadAndExtract(String link, String fileName, String destDir) throws Exception;
 }
