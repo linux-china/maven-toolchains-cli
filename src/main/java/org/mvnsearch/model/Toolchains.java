8455791453ab9cca4aeb36c0347e2f4fe8a1b4e6
linux_china <libing.chen@gmail.com>
2021-03-18 18:06:12 -0700

    Add findToolchain()

--- src/main/java/org/mvnsearch/model/Toolchains.java
+++ src/main/java/org/mvnsearch/model/Toolchains.java
@@ -2,6 +2,7 @@ package org.mvnsearch.model;
 
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,5 +18,12 @@ public class Toolchains {
 
     public void setToolchain(List<Toolchain> toolchain) {
         this.toolchain = toolchain;
+    }
+
+    public Toolchain findToolchain(String version, @Nullable String vendor) {
+        return getToolchain().stream()
+                .filter(toolchain -> toolchain.findVersion().equals(version))
+                .filter(toolchain -> vendor == null || vendor.equals(toolchain.findVendor()))
+                .findFirst().orElse(null);
     }
 }
