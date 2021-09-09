6dee6f68749fca4071d5bc84b35acb64a7a2150d
linux_china <libing.chen@gmail.com>
2021-09-09 11:39:42 -0700

    Add List all installed JDK on host

---
+++ src/main/java/org/mvnsearch/commands/ListInstalledJDK.java
@@ -0,0 +1,63 @@
+package org.mvnsearch.commands;
+
+import org.springframework.stereotype.Component;
+import org.zeroturnaround.exec.ProcessExecutor;
+import picocli.CommandLine;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.Callable;
+import java.util.stream.Stream;
+
+/**
+ * List all installed JDK on host
+ *
+ * @author linux_china
+ */
+@Component
+@CommandLine.Command(name = "all", mixinStandardHelpOptions = true, description = "List all installed JDK")
+public class ListInstalledJDK implements Callable<Integer> {
+    @Override
+    public Integer call() throws Exception {
+        final String userHome = System.getProperty("user.home");
+        List<File> installedDirs = Arrays.asList(
+                new File("/Library/Java/JavaVirtualMachines"),
+                new File(userHome, "Library/Java/JavaVirtualMachines"),
+                new File(userHome, ".sdkman/candidates/java/"),
+                new File(userHome, ".gradle/jdks"),
+                new File(userHome, ".jenv/candidates/java"),
+                new File(userHome, "..m2/jdks")
+        );
+        for (File dest : installedDirs) {
+            if (dest.exists() && dest.listFiles() != null) {
+                Stream.of(Objects.requireNonNull(dest.listFiles()))
+                        .filter(File::isDirectory)
+                        .filter(dir -> {
+                            final File javaBin1 = new File(dir, "bin/java");
+                            final File javaBin2 = new File(dir, "Contents/Home/bin/java");
+                            return javaBin1.exists() || javaBin2.exists();
+                        })
+                        .map(javaHome -> {
+                            final File javaBin1 = new File(javaHome, "bin/java");
+                            final File javaBin2 = new File(javaHome, "Contents/Home/bin/java");
+                            return javaBin1.exists() ? javaBin1 : javaBin2;
+                        })
+                        .forEach(javaBin -> {
+                            try {
+                                String javaVersionText = new ProcessExecutor().command(javaBin.getAbsolutePath(), "-version")
+                                        .readOutput(true).execute().outputUTF8().split("\n")[0];
+                                final String[] parts = javaVersionText.split("[\\s\"]+");
+                                String vendor = parts[0];
+                                String version = parts[2];
+                                System.out.println(String.format("%20s", vendor + " " + version) + " : " + javaBin.getAbsolutePath());
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        });
+            }
+        }
+        return 0;
+    }
+}
