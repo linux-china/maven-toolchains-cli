3eb98bf3f5b28720309c14d98972a0fa51bb5ec9
linux_china <libing.chen@gmail.com>
2021-10-22 14:11:02 -0700

    Add getOsName() and getArchName()

--- src/main/java/org/mvnsearch/service/impl/JdkDistributionSupport.java
+++ src/main/java/org/mvnsearch/service/impl/JdkDistributionSupport.java
@@ -65,4 +65,27 @@ public class JdkDistributionSupport {
         unArchiver.setOverwrite(true);
         unArchiver.extract();
     }
+
+    String getOsName() {
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.contains("mac")) {
+            return "mac";
+        } else if (os.contains("windows")) {
+            return "windows";
+        } else {
+            return "linux";
+        }
+    }
+
+    String getArchName() {
+        String arch = System.getProperty("os.arch").toLowerCase();
+        if (arch.contains("x86_32") || arch.contains("amd32")) {
+            arch = "x32";
+        } else if (arch.contains("aarch64") || arch.contains("arm64")) {
+            arch = "aarch64";
+        } else {
+            arch = "x64";
+        }
+        return arch;
+    }
 }
