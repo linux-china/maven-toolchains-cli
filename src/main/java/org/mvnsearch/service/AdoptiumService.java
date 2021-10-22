c9624a41c6245e15caf0f8244f8107e0a10e77c6
linux_china <libing.chen@gmail.com>
2021-10-22 13:56:34 -0700

    Polishing

--- src/main/java/org/mvnsearch/service/AdoptiumService.java
+++ src/main/java/org/mvnsearch/service/AdoptiumService.java
@@ -1,13 +1,15 @@
 package org.mvnsearch.service;
 
 
+import org.jetbrains.annotations.Nullable;
 import org.mvnsearch.model.adoptium.JdkRelease;
 
 import java.io.File;
 
 public interface AdoptiumService {
 
-    JdkRelease[] findReleases(String version) throws Exception;
+    @Nullable
+    JdkRelease findReleases(String version) throws Exception;
 
     File downloadAndExtract(String link, String fileName, String destDir) throws Exception;
 }
