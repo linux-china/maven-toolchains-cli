e9042b63040b269cb4e7f1f0d01ce094e74574ab
linux_china <libing.chen@gmail.com>
2021-10-22 14:54:17 -0700

    Add versions for vendor

--- src/main/java/org/mvnsearch/commands/ListVendors.java
+++ src/main/java/org/mvnsearch/commands/ListVendors.java
@@ -19,25 +19,25 @@ public class ListVendors implements Callable<Integer> {
     @Override
     public Integer call() {
         List<String[]> table = new ArrayList<>();
-        table.add(new String[]{"Full Name", "name"});
-        table.add(new String[]{"Adoptium", "temurin"});
-        table.add(new String[]{"AOJ", "aoj"});
-        table.add(new String[]{"AOJ OpenJ9", "corretto"});
-        table.add(new String[]{"Corretto", "corretto"});
-        table.add(new String[]{"Dragonwell", "dragonwell"});
-        table.add(new String[]{"Graal VM CE 8", "graalvm_ce8"});
-        table.add(new String[]{"Graal VM CE 11", "graalvm_ce11"});
-        table.add(new String[]{"Graal VM CE 17", "graalvm_ce17"});
-        table.add(new String[]{"Liberica", "liberica"});
-        table.add(new String[]{"Liberica Native", "liberica_native"});
-        table.add(new String[]{"Mandrel", "mandrel"});
-        table.add(new String[]{"OJDKBuild", "ojdk_build"});
-        table.add(new String[]{"Oracle", "oracle"});
-        table.add(new String[]{"Oracle OpenJDK", "oracle_openjdk"});
-        table.add(new String[]{"Red Hat", "redhat"});
-        table.add(new String[]{"SAP Machine", "sapmachine"});
-        table.add(new String[]{"Trava", "trava"});
-        table.add(new String[]{"Zulu", "zulu"});
+        table.add(new String[]{"Full Name", "name", "JDK versions"});
+        table.add(new String[]{"Adoptium", "temurin", "8, 11, 17"});
+        table.add(new String[]{"AOJ", "aoj", "8, 9, 10, 11, 12, 13, 14, 15, 16"});
+        table.add(new String[]{"AOJ OpenJ9", "aoj_openj9", "8, 9, 10, 11, 12, 13, 14, 15, 16"});
+        table.add(new String[]{"Corretto", "corretto", "8, 11, 15, 16, 17"});
+        table.add(new String[]{"Dragonwell", "dragonwell", "8, 11, 17"});
+        table.add(new String[]{"Graal VM CE 8", "graalvm_ce8", "19.3, 20, 20.1, 20.2, 20.3, 21, 21.1, 21.2"});
+        table.add(new String[]{"Graal VM CE 11", "graalvm_ce11", "19.3, 20, 20.1, 20.2, 20.3, 21, 21.1, 21.2, 21.3"});
+        table.add(new String[]{"Graal VM CE 17", "graalvm_ce17", "21.3.0"});
+        table.add(new String[]{"Liberica", "liberica", "8, 9, 10, 11, 12, 13, 14, 15, 16, 17"});
+        table.add(new String[]{"Liberica Native", "liberica_native", "21.0, 21.1, 21.2"});
+        table.add(new String[]{"Mandrel", "mandrel", "20.1, 20.2, 20.3, 21, 21.1, 21.2, 21.3"});
+        table.add(new String[]{"OJDKBuild", "ojdk_build", "8, 9, 10, 11, 12, 13, 14, 15"});
+        table.add(new String[]{"Oracle", "oracle", "6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17"});
+        table.add(new String[]{"Oracle OpenJDK", "oracle_openjdk", "8, 9, 10, 11, 12, 13, 14, 15, 16, 17"});
+        table.add(new String[]{"Red Hat", "redhat", "8, 9, 10, 11, 12, 13, 14, 15"});
+        table.add(new String[]{"SAP Machine", "sapmachine", "10, 11, 12, 13, 14, 15, 16, 17"});
+        table.add(new String[]{"Trava", "trava", "8, 11"});
+        table.add(new String[]{"Zulu", "zulu", "6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17"});
         FormatUtil.tableWithLines(table);
         return 0;
     }
