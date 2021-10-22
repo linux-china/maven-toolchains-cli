7240c2c2d3a638cc3a13d4c8d36c0ef5e035ec94
linux_china <libing.chen@gmail.com>
2021-10-22 13:20:44 -0700

    Use plexus-archiver to decompress zip file

--- src/main/java/org/mvnsearch/service/impl/AdoptOpenJDKServiceImpl.java
+++ src/main/java/org/mvnsearch/service/impl/AdoptOpenJDKServiceImpl.java
@@ -3,19 +3,21 @@ package org.mvnsearch.service.impl;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.compress.archivers.ArchiveInputStream;
-import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
 import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream;
 import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
-import org.apache.commons.compress.utils.IOUtils;
 import org.apache.commons.io.FileUtils;
+import org.codehaus.plexus.archiver.AbstractUnArchiver;
+import org.codehaus.plexus.archiver.tar.TarGZipUnArchiver;
+import org.codehaus.plexus.archiver.zip.ZipUnArchiver;
+import org.codehaus.plexus.logging.Logger;
+import org.codehaus.plexus.logging.console.ConsoleLogger;
 import org.mvnsearch.model.jdk.JdkRelease;
 import org.mvnsearch.service.AdoptOpenJDKService;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 
@@ -34,26 +36,6 @@ public class AdoptOpenJDKServiceImpl implements AdoptOpenJDKService {
         }
         URL link = new URL("https://api.adoptium.net/v3/assets/latest/" + majorVersion + "/hotspot?release=latest&jvm_impl=hotspot&vendor=adoptium");
         return objectMapper.readValue(link, JdkRelease[].class);
-    }
-
-    @Override
-    public File downloadAndExtract(String link, String fileName, String destDir) throws Exception {
-        File destFile = new File(destDir, fileName);
-        if (!destFile.exists()) {
-            System.out.println("Begin to download: " + link);
-            FileUtils.copyURLToFile(new URL(link), destFile);
-        }
-        String extractDir = getRootNameInArchive(destFile);
-        File jdkDir = new File(destDir, extractDir);
-        if (!jdkDir.exists()) {
-            if (fileName.endsWith(".tgz") || fileName.endsWith(".tar.gz")) {
-                new ProcessBuilder().directory(new File(destDir)).command("tar", "zxpf", fileName).start();
-            } else {
-                new ProcessBuilder().directory(new File(destDir)).command("unzip", fileName).start();
-            }
-            // decompress(destFile, new File(destDir));
-        }
-        return new File(destDir, extractDir);
     }
 
     public String getRootNameInArchive(File archiveFile) throws Exception {
@@ -77,25 +59,31 @@ public class AdoptOpenJDKServiceImpl implements AdoptOpenJDKService {
         return name;
     }
 
-    //with file permission reserved
-    public void decompress(File in, File out) throws IOException {
-        try (TarArchiveInputStream fin = new TarArchiveInputStream(new GzipCompressorInputStream(new FileInputStream(in)))) {
-            TarArchiveEntry entry;
-            while ((entry = fin.getNextTarEntry()) != null) {
-                if (entry.isDirectory()) {
-                    continue;
-                }
-                File curFile = new File(out, entry.getName());
-                if (entry.getName().endsWith("javac")) {
-                    System.out.println("javac mode: " + entry.getMode());
-                    curFile.setExecutable(true);
-                }
-                File parent = curFile.getParentFile();
-                if (!parent.exists()) {
-                    parent.mkdirs();
-                }
-                IOUtils.copy(fin, new FileOutputStream(curFile));
-            }
+    @Override
+    public File downloadAndExtract(String link, String fileName, String destDir) throws Exception {
+        File destFile = new File(destDir, fileName);
+        if (!destFile.exists()) {
+            System.out.println("Begin to download: " + link);
+            FileUtils.copyURLToFile(new URL(link), destFile);
         }
+        System.out.println("Begin to extract: " + fileName);
+        String extractDir = getRootNameInArchive(destFile);
+        extractArchiveFile(destFile, new File(destDir));
+        return new File(destDir, extractDir);
+    }
+
+    public void extractArchiveFile(File sourceFile, File destDir) throws IOException {
+        String fileName = sourceFile.getName();
+        final AbstractUnArchiver unArchiver;
+        if (fileName.endsWith(".tgz") || fileName.endsWith(".tar.gz")) {
+            unArchiver = new TarGZipUnArchiver();
+        } else {
+            unArchiver = new ZipUnArchiver();
+        }
+        unArchiver.enableLogging(new ConsoleLogger(Logger.LEVEL_ERROR, "console"));
+        unArchiver.setSourceFile(sourceFile);
+        unArchiver.setDestDirectory(destDir);
+        unArchiver.setOverwrite(true);
+        unArchiver.extract();
     }
 }
