8d7981da78fa7d55b86f289d6707a9771e50f4a0
linux_china <libing.chen@gmail.com>
2021-10-22 15:14:51 -0700

    Add graalvm 17

---
+++ src/test/java/org/mvnsearch/service/impl/FoojayServiceImplTest.java
@@ -0,0 +1,15 @@
+package org.mvnsearch.service.impl;
+
+
+import org.junit.jupiter.api.Test;
+import org.mvnsearch.service.JdkDownloadLink;
+
+public class FoojayServiceImplTest {
+    private FoojayServiceImpl foojayService = new FoojayServiceImpl();
+
+    @Test
+    public void testFindJDKReleases() throws Exception {
+        final JdkDownloadLink jdkLink = foojayService.findRelease("11", "oracle_openjdk");
+        System.out.println(jdkLink.getUrl() + ":" + jdkLink.getFileName());
+    }
+}
