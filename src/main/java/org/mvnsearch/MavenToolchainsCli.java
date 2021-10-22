bd19d7d6ecd4f7620444a11201f40ef1dcc63729
linux_china <libing.chen@gmail.com>
2021-10-22 13:23:58 -0700

    Polishing

--- src/main/java/org/mvnsearch/MavenToolchainsCli.java
+++ src/main/java/org/mvnsearch/MavenToolchainsCli.java
@@ -2,10 +2,10 @@ package org.mvnsearch;
 
 import org.mvnsearch.model.Toolchain;
 import org.mvnsearch.model.Toolchains;
-import org.mvnsearch.model.jdk.JdkBinary;
-import org.mvnsearch.model.jdk.JdkPackage;
-import org.mvnsearch.model.jdk.JdkRelease;
-import org.mvnsearch.model.jdk.JdkVersion;
+import org.mvnsearch.model.adoptium.JdkBinary;
+import org.mvnsearch.model.adoptium.JdkPackage;
+import org.mvnsearch.model.adoptium.JdkRelease;
+import org.mvnsearch.model.adoptium.JdkVersion;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
