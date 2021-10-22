76fdab3ed34b206bd599e95f9048d3ba88583d7c
linux_china <libing.chen@gmail.com>
2021-10-21 18:55:36 -0700

    Add testPrintDownloadUrl

--- src/test/java/org/mvnsearch/DiscoClientTest.java
+++ src/test/java/org/mvnsearch/DiscoClientTest.java
@@ -21,8 +21,8 @@ public class DiscoClientTest {
         final List<Distribution> distributionsForVersion = discoClient.getDistributionsForVersion(new VersionNumber(11));
         //openjdk-11.0.2_osx-x64_bin.tar.gz
         final List<Pkg> pkgs = discoClient.getPkgs(
-                Distribution.ORACLE_OPEN_JDK,
-                new VersionNumber(11),
+                Distribution.ORACLE,
+                new VersionNumber(17),
                 Latest.OVERALL,
                 OperatingSystem.MACOS,
                 LibCType.LIBC,
@@ -39,12 +39,14 @@ public class DiscoClientTest {
             System.out.println(pkg);
             System.out.println(pkg.getId());
         });
-        //pkg for openjdk-11.0.2_osx-x64_bin.tar.gz
-        final Pkg pkg = discoClient.getPkg("04fe3c5dcd0c4a21fb8f05ceb33734f8");
-        //pkg download url
+    }
+
+    @Test
+    public void testPrintDownloadUrl() {
+        String pkgId = "5b43d63862207af0091ab9b6cfb0d4c0";
+        final Pkg pkg = discoClient.getPkg(pkgId);
         final String pkgDirectDownloadUri = discoClient.getPkgDirectDownloadUri(pkg.getEphemeralId(), new SemVer(pkg.getDistributionVersion()));
         System.out.println(pkgDirectDownloadUri);
-
     }
 
     @Test
