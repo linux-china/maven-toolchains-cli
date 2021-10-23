96ff2438158c0f009cdff58fa5129a9ece3a4f95
linux_china <libing.chen@gmail.com>
2021-10-22 21:10:44 -0700

    Add profiles

--- src/main/java/org/mvnsearch/commands/MavenToolchainsPlugin.java
+++ src/main/java/org/mvnsearch/commands/MavenToolchainsPlugin.java
@@ -24,50 +24,78 @@ public class MavenToolchainsPlugin implements Callable<Integer> {
 
     @Override
     public Integer call() throws Exception {
-        String pluginXml = null;
+        String pluginXml;
         if (vendor != null) {
             //language=xml
-            pluginXml = "<plugin>\n" +
-                    "    <groupId>org.apache.maven.plugins</groupId>\n" +
-                    "    <artifactId>maven-toolchains-plugin</artifactId>\n" +
-                    "    <version>3.0.0</version>\n" +
-                    "    <executions>\n" +
-                    "        <execution>\n" +
-                    "            <goals>\n" +
-                    "                <goal>toolchain</goal>\n" +
-                    "            </goals>\n" +
-                    "        </execution>\n" +
-                    "    </executions>\n" +
-                    "    <configuration>\n" +
-                    "        <toolchains>\n" +
-                    "            <jdk>\n" +
-                    "                <version>" + version + "</version>\n" +
-                    "                <vendor>" + vendor + "</vendor>\n" +
-                    "            </jdk>\n" +
-                    "        </toolchains>\n" +
-                    "    </configuration>\n" +
-                    "</plugin>";
+            pluginXml = "<profiles>\n" +
+                    "    <profile>\n" +
+                    "        <id>toolchains</id>\n" +
+                    "        <activation>\n" +
+                    "            <file>\n" +
+                    "                <exists>${env.HOME}/.m2/toolchains.xml</exists>\n" +
+                    "            </file>\n" +
+                    "        </activation>\n" +
+                    "        <build>\n" +
+                    "            <plugins>\n" +
+                    "                <plugin>\n" +
+                    "                    <groupId>org.apache.maven.plugins</groupId>\n" +
+                    "                    <artifactId>maven-toolchains-plugin</artifactId>\n" +
+                    "                    <version>3.0.0</version>\n" +
+                    "                    <executions>\n" +
+                    "                        <execution>\n" +
+                    "                            <goals>\n" +
+                    "                                <goal>toolchain</goal>\n" +
+                    "                            </goals>\n" +
+                    "                        </execution>\n" +
+                    "                    </executions>\n" +
+                    "                    <configuration>\n" +
+                    "                        <toolchains>\n" +
+                    "                            <jdk>\n" +
+                    "                                <version>" + version + "</version>\n" +
+                    "                                <version>" + vendor + "</version>\n" +
+                    "                            </jdk>\n" +
+                    "                        </toolchains>\n" +
+                    "                    </configuration>\n" +
+                    "                </plugin>\n" +
+                    "            </plugins>\n" +
+                    "        </build>\n" +
+                    "    </profile>\n" +
+                    "</profiles>";
         } else {
             //language=xml
-            pluginXml = "<plugin>\n" +
-                    "    <groupId>org.apache.maven.plugins</groupId>\n" +
-                    "    <artifactId>maven-toolchains-plugin</artifactId>\n" +
-                    "    <version>3.0.0</version>\n" +
-                    "    <executions>\n" +
-                    "        <execution>\n" +
-                    "            <goals>\n" +
-                    "                <goal>toolchain</goal>\n" +
-                    "            </goals>\n" +
-                    "        </execution>\n" +
-                    "    </executions>\n" +
-                    "    <configuration>\n" +
-                    "        <toolchains>\n" +
-                    "            <jdk>\n" +
-                    "                <version>" + version + "</version>\n" +
-                    "            </jdk>\n" +
-                    "        </toolchains>\n" +
-                    "    </configuration>\n" +
-                    "</plugin>";
+            pluginXml = "<profiles>\n" +
+                    "    <profile>\n" +
+                    "        <id>toolchains</id>\n" +
+                    "        <activation>\n" +
+                    "            <file>\n" +
+                    "                <exists>${env.HOME}/.m2/toolchains.xml</exists>\n" +
+                    "            </file>\n" +
+                    "        </activation>\n" +
+                    "        <build>\n" +
+                    "            <plugins>\n" +
+                    "                <plugin>\n" +
+                    "                    <groupId>org.apache.maven.plugins</groupId>\n" +
+                    "                    <artifactId>maven-toolchains-plugin</artifactId>\n" +
+                    "                    <version>3.0.0</version>\n" +
+                    "                    <executions>\n" +
+                    "                        <execution>\n" +
+                    "                            <goals>\n" +
+                    "                                <goal>toolchain</goal>\n" +
+                    "                            </goals>\n" +
+                    "                        </execution>\n" +
+                    "                    </executions>\n" +
+                    "                    <configuration>\n" +
+                    "                        <toolchains>\n" +
+                    "                            <jdk>\n" +
+                    "                                <version>" + version + "</version>\n" +
+                    "                            </jdk>\n" +
+                    "                        </toolchains>\n" +
+                    "                    </configuration>\n" +
+                    "                </plugin>\n" +
+                    "            </plugins>\n" +
+                    "        </build>\n" +
+                    "    </profile>\n" +
+                    "</profiles>";
         }
         System.out.println(pluginXml);
         return 0;
