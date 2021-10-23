0c966036db28fcede4e841358f3e1087d13f4f7d
linux_china <libing.chen@gmail.com>
2021-10-23 10:04:04 -0700

    Add testFindGraalVMReleases

--- src/test/java/org/mvnsearch/service/impl/FoojayServiceImplTest.java
+++ src/test/java/org/mvnsearch/service/impl/FoojayServiceImplTest.java
@@ -12,4 +12,10 @@ public class FoojayServiceImplTest {
         final JdkDownloadLink jdkLink = foojayService.findRelease("11", "zulu");
         System.out.println(jdkLink.getUrl() + ":  " + jdkLink.getFileName());
     }
+
+    @Test
+    public void testFindGraalVMReleases() throws Exception {
+        final JdkDownloadLink jdkLink = foojayService.findRelease("21.2", "graalvm_ce11");
+        System.out.println(jdkLink.getUrl() + ":  " + jdkLink.getFileName());
+    }
 }
