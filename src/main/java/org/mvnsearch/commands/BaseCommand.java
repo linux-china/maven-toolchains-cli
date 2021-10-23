2f62b606c4203cd0865d37a071c7294421fd9a09
linux_china <libing.chen@gmail.com>
2021-10-23 11:01:21 -0700

    Remove getOsName and getArchName

--- src/main/java/org/mvnsearch/commands/BaseCommand.java
+++ src/main/java/org/mvnsearch/commands/BaseCommand.java
@@ -6,26 +6,5 @@ package org.mvnsearch.commands;
  * @author linux_china
  */
 public interface BaseCommand {
-    default String getOsName() {
-        String os = System.getProperty("os.name").toLowerCase();
-        if (os.contains("mac")) {
-            return "mac";
-        } else if (os.contains("windows")) {
-            return "windows";
-        } else {
-            return "linux";
-        }
-    }
 
-    default String getArchName() {
-        String arch = System.getProperty("os.arch").toLowerCase();
-        if (arch.contains("x86_32") || arch.contains("amd32")) {
-            arch = "x32";
-        } else if (arch.contains("aarch64") || arch.contains("arm64")) {
-            arch = "aarch64";
-        } else {
-            arch = "x64";
-        }
-        return arch;
-    }
 }
