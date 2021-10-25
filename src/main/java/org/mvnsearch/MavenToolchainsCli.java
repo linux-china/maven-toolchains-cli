43feb565d072047f80e1f8535e3dd0a967377b8c
linux_china <libing.chen@gmail.com>
2021-10-25 11:19:05 -0700

    Polishing

--- src/main/java/org/mvnsearch/MavenToolchainsCli.java
+++ src/main/java/org/mvnsearch/MavenToolchainsCli.java
@@ -3,10 +3,6 @@ package org.mvnsearch;
 import org.mvnsearch.commands.ListVendors;
 import org.mvnsearch.model.Toolchain;
 import org.mvnsearch.model.Toolchains;
-import org.mvnsearch.model.adoptium.JdkBinary;
-import org.mvnsearch.model.adoptium.JdkPackage;
-import org.mvnsearch.model.adoptium.JdkRelease;
-import org.mvnsearch.model.adoptium.JdkVersion;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,8 +11,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.nativex.hint.TypeHint;
 
 @SpringBootApplication(proxyBeanMethods = false)
-@TypeHint(types = {Toolchains.class, Toolchain.class,
-        JdkRelease.class, JdkBinary.class, JdkPackage.class, JdkVersion.class, ListVendors.class})
+@TypeHint(types = {Toolchains.class, Toolchain.class, ListVendors.class})
 public class MavenToolchainsCli {
     public static void main(String[] args) {
         ConfigurableApplicationContext app = new SpringApplicationBuilder(MavenToolchainsCli.class)
