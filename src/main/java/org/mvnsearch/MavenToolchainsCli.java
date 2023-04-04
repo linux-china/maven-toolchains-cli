8b08edf32e88e26aabd703c52094b2167471394a
Libing Chen <libing.chen@gmail.com>
2023-04-04 16:18:44 +0800

    feat: add ImportRuntimeHints

--- src/main/java/org/mvnsearch/MavenToolchainsCli.java
+++ src/main/java/org/mvnsearch/MavenToolchainsCli.java
@@ -1,17 +1,15 @@
 package org.mvnsearch;
 
-import org.mvnsearch.commands.ListVendors;
-import org.mvnsearch.model.Toolchain;
-import org.mvnsearch.model.Toolchains;
+import org.mvnsearch.aot.ProjectRuntimeHints;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.nativex.hint.TypeHint;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication(proxyBeanMethods = false)
-@TypeHint(types = {Toolchains.class, Toolchain.class, ListVendors.class})
+@ImportRuntimeHints(ProjectRuntimeHints.class)
 public class MavenToolchainsCli {
     public static void main(String[] args) {
         ConfigurableApplicationContext app = new SpringApplicationBuilder(MavenToolchainsCli.class)
